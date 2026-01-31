a27992b070a21c8b2d1def0337db95bab1aeecbb
MrMico <63750809+MrMico@users.noreply.github.com>
2026-01-31 18:48:39 -0500

    makes noises now

--- main/main.ino
+++ main/main.ino
@@ -1,10 +1,43 @@
-//Main
-void setup() {
-  // put your setup code here, to run once:
+/*
+ * Created by ArduinoGetStarted.com
+ *
+ * This example code is in the public domain
+ *
+ * Tutorial page: https://arduinogetstarted.com/tutorials/arduino-piezo-buzzer
+ */
 
+#include "pitches.h"
+
+#define BUZZER_PIN 11 // The Arduino pin connected to the buzzer
+
+// notes in the melody:
+int melody[] = {
+  NOTE_C4, NOTE_G3, NOTE_G3, NOTE_A3, NOTE_G3, 0, NOTE_B3, NOTE_C4
+};
+
+// note durations: 4 = quarter note, 8 = eighth note, etc.:
+int noteDurations[] = {
+  4, 8, 8, 4, 4, 4, 4, 4
+};
+
+void setup() {
+  // iterate over the notes of the melody:
+  for (int thisNote = 0; thisNote < 8; thisNote++) {
+
+    // to calculate the note duration, take one second divided by the note type.
+    //e.g. quarter note = 1000 / 4, eighth note = 1000/8, etc.
+    int noteDuration = 1000 / noteDurations[thisNote];
+    tone(BUZZER_PIN, melody[thisNote], noteDuration);
+
+    // to distinguish the notes, set a minimum time between them.
+    // the note's duration + 30% seems to work well:
+    int pauseBetweenNotes = noteDuration * 1.30;
+    delay(pauseBetweenNotes);
+    // stop the tone playing:
+    noTone(BUZZER_PIN);
+  }
 }
 
 void loop() {
-  // put your main code here, to run repeatedly:
-
+  // no need to repeat the melody.
 }
